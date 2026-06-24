d0baf966095cb7f279842c514cbb7a4b8d15eb47
aarav2pf <aaravsadhu7@gmail.com>
2026-06-24 20:27:06 +0530

    initial pokedex structure commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Header } from "./components/Header";
+import { PokeCard } from "./components/PokeCard";
+import { SideNav } from "./components/SideNav";
+
+import { useState } from "react";
+
+export default function App() {
+  const [selectedPokemon, setSelectedPokemon] = useState(0);
+
+
+  return (
+    <>
+      <Header />
+      <SideNav selectedPokemon={selectedPokemon} setSelectedPokemon={setSelectedPokemon}/>
+      <PokeCard selectedPokemon={selectedPokemon}/>
+    </>
+  )
+}
+
---
+++ src/components/PokeCard.jsx
@@ -0,0 +1,50 @@
+import { useEffect } from "react";
+import { UseEffect } from "react";
+
+export function PokeCard(props) {
+    const { selelectedPokemon } = props;
+    const [data, setData] = useState(null); 
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (loading || !localStorage) { return; } 
+        let cache = {};
+        if (localStorage.getItem('pokedex')) {
+            cache = JSON.parse(localStorage.getItem('pokedex'));
+        }
+
+        if(selectedPokemon in cache) {
+            setData(cache[selectedPokemon]);
+            return;
+        }
+
+        async function fetchPokemonData() {
+            setLoading(true);
+            try {
+                const baseUrl = 'https://pokeapi.co/api/v2/';
+                const suffix = 'pokemon/' + selectedPokemon;
+                const final = baseUrl + suffix;
+                const res = await fetch(finalUrl);
+                const pokemonData = await res.json();
+                setData(pokemonData)
+
+                cache[selectedPokemon] = pokemonData;
+                localStorage.setItem(JSON.stringify(cache));
+
+            } catch (err) {
+                console.log(err.message);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchPokemonData();
+
+    }, [selelectedPokemon])
+
+    return (
+        <div>
+
+        </div>
+    )
+}
