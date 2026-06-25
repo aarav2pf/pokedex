77388fe62b05799d68920025cae886235c896cfe
aarav2pf <aaravsadhu7@gmail.com>
2026-06-25 14:37:57 +0530

    pokecard and sidenav updated

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,13 +1,21 @@
 import { useEffect, useState } from "react";
 import { getFullPokedexNumber, getPokedexNumber } from "../utils";
 import TypeCard from "./TypeCard";
+import Modal from "./Modal";
 
 export default function PokeCard(props) {
     const { selectedPokemon } = props;
     const [data, setData] = useState(null); 
     const [loading, setLoading] = useState(false);
+    const [skill, setSkill] = useState(null);
 
     const {name, height, abilities, stats, types, moves, sprites} = data || {};
+
+    const imgList = Object.keys(sprites || {}).filter(val => {
+        if (!sprites[val]) { return  false }
+        if(['versions','other'].includes(val)) { return false }
+        return true
+    })
 
     useEffect(() => {
         if (loading || !localStorage) { return; } 
@@ -56,6 +64,18 @@ export default function PokeCard(props) {
 
     return (
         <div className='poke-card'>
+           {skill && (
+                <Modal handleCloseModal={() => {}}>
+                    <div>
+                        <h6>Name</h6>
+                        <h2></h2>
+                    </div>
+                    <div>
+                        <h6>Description</h6>
+                        <p></p>
+                    </div>
+                </Modal>
+            )}
             <div>
                 <h4>#{getFullPokedexNumber(selectedPokemon)}</h4>
                 <h2>{name}</h2>
@@ -64,6 +84,39 @@ export default function PokeCard(props) {
                 {types.map((typeObj, typeIndex) => {
                     return (
                         <TypeCard key={typeIndex} type={typeObj?.type?.name} />
+                    )
+                })}
+            </div>
+
+            <img className='default-img' src={'/pokemon/' + getFullPokedexNumber(selectedPokemon) + '.png'} alt={`${name}-large-img`} />
+
+            <div className='img-container'>
+                {imgList.map((spriteUrl, spriteIndex) => {
+                    const imgUrl = sprites[spriteUrl]
+                    return (
+                        <img key={spriteIndex} src = {imgUrl} alt = {`${name}-img-${spriteUrl}`} />
+                    )
+                })}
+            </div>
+            <h3>Stats</h3>
+            <div className='stats-card'>
+                {stats.map((statObj, statIndex) => {
+                    const {stat, base_stat} = statObj
+                    return (
+                        <div key={statIndex} className='stat-item'>
+                            <p>{stat?.name.replaceAll('-',' ')}</p>
+                            <h4>{base_stat}</h4>
+                        </div>
+                    )
+                })}
+            </div>
+            <h3>Moves</h3>
+            <div className='pokemon-move-grid'>
+                {moves.map((moveObj, moveIndex) => {
+                    return (
+                        <button className='button-card pokemon-move' key={moveIndex} onClick={() => {}}>
+                        <p>{moveObj?.move?.name?.replaceAll('-',' ')}</p>
+                        </button>
                     )
                 })}
             </div>
